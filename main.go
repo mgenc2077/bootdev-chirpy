2e13f6fb48c2af10342ecb71f8a62a98e77c7707
mgenc2077 <mgenc404@gmail.com>
2025-03-10 18:13:58 +0300

    CH2:Routing L3:Routing

--- main.go
+++ main.go
@@ -22,19 +22,19 @@ func main() {
 	asd := &apiconfig{}
 	mux.Handle("/app/", asd.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		hitcount := asd.fileserverHits.Load()
 		w.Write([]byte(fmt.Sprintf("Hits: %v", hitcount)))
 	})
-	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		asd.fileserverHits.Store(0)
 		w.Write([]byte(fmt.Sprintf("Hits: %v", asd.fileserverHits.Load())))
 	})
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
