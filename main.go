b58357c1b69489e8a3d6db2195518dba12ea5898
mgenc2077 <mgenc404@gmail.com>
2025-03-12 00:16:57 +0300

    CH4:JSON L6:The Profane

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
 type apiconfig struct {
 	fileserverHits atomic.Int32
+}
+type errordata struct {
+	Error string `json:"error"`
+}
+type jsondata struct {
+	Body string `json:"body"`
+}
+type jsonresponse struct {
+	Cleaned_body string `json:"cleaned_body"`
 }
 
 func (cfg *apiconfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -16,6 +26,35 @@ func (cfg *apiconfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func returnwitherror(w http.ResponseWriter, code int, msg string) int {
+	w.Header().Set("Content-Type", "application/json")
+	check := 1
+	errResp := errordata{Error: msg}
+	errjson, _ := json.Marshal(errResp)
+	w.WriteHeader(code)
+	w.Write(errjson)
+	return check
+}
+
+func returnwithvalues(w http.ResponseWriter, code int, bodydata jsondata) {
+	w.Header().Set("Content-Type", "application/json")
+	arr := strings.Split(bodydata.Body, " ")
+	var arres []string
+	for _, v := range arr {
+		val1 := strings.ToLower(v)
+		if (val1 == "kerfuffle") || (val1 == "sharbert") || (val1 == "fornax") {
+			arres = append(arres, "****")
+			continue
+		}
+		arres = append(arres, v)
+	}
+	rspstring := strings.Join(arres, " ")
+	response := jsonresponse{Cleaned_body: rspstring}
+	rspjson, _ := json.Marshal(response)
+	w.WriteHeader(code)
+	w.Write(rspjson)
 }
 
 func main() {
@@ -46,43 +85,19 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-		type jsondata struct {
-			Body string `json:"body"`
-		}
-		type errordata struct {
-			Error string `json:"error"`
-		}
-		type rspvalid struct {
-			Valid bool `json:"valid"`
-		}
 		decoder := json.NewDecoder(r.Body)
 		params := jsondata{}
 		var check = 0
 		err := decoder.Decode(&params)
 		if err != nil {
-			w.Header().Set("Content-Type", "application/json")
-			errResp := errordata{Error: "Something went wrong"}
-			errjson, _ := json.Marshal(errResp)
-			w.WriteHeader(500)
-			w.Write(errjson)
-			check = 1
+			check = returnwitherror(w, 500, "Something went wrong")
 		}
 		if len(params.Body) > 140 {
-			w.Header().Set("Content-Type", "application/json")
-			errlen := errordata{Error: "Chirp is too long"}
-			errlenjson, _ := json.Marshal(errlen)
-			w.WriteHeader(400)
-			w.Write(errlenjson)
-			check = 1
+			check = returnwitherror(w, 400, "Chirp is too long")
 		}
 		if check == 0 {
-			w.Header().Set("Content-Type", "application/json")
-			corres := rspvalid{Valid: true}
-			corresjson, _ := json.Marshal(corres)
-			w.WriteHeader(200)
-			w.Write(corresjson)
+			returnwithvalues(w, 200, params)
 		}
-
 	})
 	server := http.Server{
 		Addr:    ":8080",
