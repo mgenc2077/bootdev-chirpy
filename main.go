fc16d83980fa588f5d9bf6667c21ea2e9df409ad
mgenc2077 <mgenc404@gmail.com>
2025-03-11 18:34:17 +0300

    CH4:JSON L2:JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -43,6 +44,45 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type jsondata struct {
+			Body string `json:"body"`
+		}
+		type errordata struct {
+			Error string `json:"error"`
+		}
+		type rspvalid struct {
+			Valid bool `json:"valid"`
+		}
+		decoder := json.NewDecoder(r.Body)
+		params := jsondata{}
+		var check = 0
+		err := decoder.Decode(&params)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			errResp := errordata{Error: "Something went wrong"}
+			errjson, _ := json.Marshal(errResp)
+			w.WriteHeader(500)
+			w.Write(errjson)
+			check = 1
+		}
+		if len(params.Body) > 140 {
+			w.Header().Set("Content-Type", "application/json")
+			errlen := errordata{Error: "Chirp is too long"}
+			errlenjson, _ := json.Marshal(errlen)
+			w.WriteHeader(400)
+			w.Write(errlenjson)
+			check = 1
+		}
+		if check == 0 {
+			w.Header().Set("Content-Type", "application/json")
+			corres := rspvalid{Valid: true}
+			corresjson, _ := json.Marshal(corres)
+			w.WriteHeader(200)
+			w.Write(corresjson)
+		}
+
 	})
 	server := http.Server{
 		Addr:    ":8080",
